1fb4fa7bcaf80e48c25583fb035e1f9b94c2afea
JanithNanayakkaraSE0612 <gayanivitharana0612@gmail.com>
2024-12-30 22:32:37 +0530

    update new details

--- utils/data/educations.js
+++ utils/data/educations.js
@@ -1,20 +1,20 @@
 export const educations = [
   {
     id: 1,
-    title: "Bachelor Degree",
-    duration: "2020 - Present",
-    institution: "National University of Bangladesh",
+    title: "Graduate Diploma Software Enigneering (GDSE)",
+    duration: "2021 - 2024",
+    institution: "Institute of Software Enigneering (IJSE)",
   },
   {
     id: 2,
-    title: "Higher Secondary Certificate",
-    duration: "2018 - 2020",
-    institution: "Noakhali Islamia Kamil Madrasah",
+    title: "G.C.E Advanced Level Examination ",
+    duration: "2020",
+    institution: "Ga/ Karandeniya Central College",
   },
   {
     id: 3,
-    title: "Secondary School Certificate",
-    duration: "2008 - 2018",
-    institution: "Baitus Saif Islamia Madrasah",
+    title: "G.C.E Ordinary Level Examination ",
+    duration: "2017",
+    institution: "Ga/ Yatagala MadhayMaha Viddyaaya",
   }
 ]
