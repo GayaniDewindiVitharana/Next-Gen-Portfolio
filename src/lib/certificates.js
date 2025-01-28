148f9eaf59eed3c8006fedad8bd44660bc202d33
JanithNanayakkaraSE0612 <gayanivitharana0612@gmail.com>
2025-01-28 21:29:59 +0530

    update

--- src/lib/certificates.js
+++ src/lib/certificates.js
@@ -1,70 +1,65 @@
 // certificates.js
-import JavaHR from "../assets/Certificate/Android_Application_GL.png";
-import ProblemSL from "../assets/Certificate/Angular_GL.png";
-import UOMPY from "../assets/Certificate/Billd_Game_JavaStript.png";
-import UOMWeb from "../assets/Certificate/client-server UOM.png";
-import DataScience from "../assets/Certificate/Coding_Eng_GL.png"
-import JQuery from "../assets/Certificate/CSS_Oppetion.png"
-import PHP from "../assets/Certificate/Data_Strat_GL.png"
-import Angular from "../assets/Certificate/DesingThinking_GL.png"
-import DevTown from "../assets/Certificate/DevOps_GL.png"
-import DevTown1 from "../assets/Certificate/Devlown.png"
-import Python from "../assets/Certificate/Dow01.png"
-import Front from "../assets/Certificate/Frond-End WebDevelopment UOM.png"
-import Game from "../assets/Certificate/Game_GL.png"
-import Javascript from "../assets/Certificate/greatleaning-java.jpeg"
-import CodingForMarkert from "../assets/Certificate/greatleaning-oop.jpeg"
-import ReactAndRedux from "../assets/Certificate/greatlening-css.jpeg"
-import ResponsiveWebDesign from "../assets/Certificate/grUxUi.png.jpeg"
-import SQL from "../assets/Certificate/HTTML_TAG_GL.png"
-import IntroductionTOJava from "../assets/Certificate/Introduction to JavaScriptsolo.png"
-import HackerJava from "../assets/Certificate/java Basic HackerRank.png"
-import SQLSOLO from "../assets/Certificate/JavaApplication.png"
-import HackJavaScript from "../assets/Certificate/javaScriptHackerRank.png"
-import JsSolo from "../assets/Certificate/jQuriy_GL.png"
-import CSSSOLO from "../assets/Certificate/Mongodb Tutorial.png"
-import JavaSolo from "../assets/CertificateWebDesingn UOM.png"
-import WebDev from "../assets/Certificate/salolearn-java.jpg"
+import webDesingn from "../assets/Certificate/WebDesingn_UOM.png";
 import simple from "../assets/Certificate/simpleUi.png"
-import solo from "../assets/Certificate/sololearn-html.jpg"
+import HackerJava from "../assets/Certificate/java Basic HackerRank.png"
+import AndroidApplication from "../assets/Certificate/Android_Application_GL.png";
+import Frond from "../assets/Certificate/Frond-End WebDevelopment UOM.png"
 import solosql from "../assets/Certificate/sololearn-SQL.jpeg"
+import uiux from "../assets/Certificate/grUxUi.png.jpeg"
 import sqlhack from "../assets/Certificate/sqlHackerRank.jpeg"
+import AngularGl from "../assets/Certificate/Angular_GL.png";
+import DevTown1 from "../assets/Certificate/Devlown.png"
+import DevTown2 from "../assets/Certificate/Dow01.png"
+import solo from "../assets/Certificate/sololearn-html.jpg"
+import Billd_Game from "../assets/Certificate/Billd_Game_JavaStript.png";
+import DesingThinking from "../assets/Certificate/DesingThinking_GL.png"
+import HackJavaScript from "../assets/Certificate/javaScriptHackerRank.png"
+import salolernJava from "../assets/Certificate/salolearn-java.jpg"
+import UOMWeb from "../assets/Certificate/client-server UOM.png";
+import Jquriy from "../assets/Certificate/jQuriy_GL.png"
+import greatleaningoop from "../assets/Certificate/greatleaning-oop.jpeg"
+import DataStrat from "../assets/Certificate/Data_Strat_GL.png"
+import JavaApplication from "../assets/Certificate/JavaApplication.png"
+import HTMLTag from "../assets/Certificate/HTTML_TAG_GL.png"
+import JavaSctriptsolo from "../assets/Certificate/Introduction to JavaScriptsolo.png"
+import coding from "../assets/Certificate/Coding_Eng_GL.png"
+import DevOpsGl from "../assets/Certificate/DevOps_GL.png"
+import Mongodb from "../assets/Certificate/Mongodb Tutorial.png"
+import Game from "../assets/Certificate/Game_GL.png"
+import greatleaningcss from "../assets/Certificate/greatlening-css.jpeg"
+import greatleaningjava from "../assets/Certificate/greatleaning-java.jpeg"
 import vistion from "../assets/Certificate/Vistion_GL.png"
 
+
 export const certificates = [
-  { id: 1, title: "Java", date: "14 July 2022", image: JavaHR },
-  { id: 2, title: "Problem Solving", date: "15 March 2023", image: ProblemSL },
-  { id: 3, title: "Python", date: "24 October 2022", image: UOMPY },
-  { id: 4, title: "Web Designer for Beginner", date: "16 August 2024", image: UOMWeb },
-  { id: 5, title: "Data Science", date: "20 September 2024", image: DataScience },
-  { id: 6, title: "JQuery", date: "12 October 2023", image: JQuery },
-  { id: 7, title: "PHP", date: "10 November 2023", image: PHP },
-  { id: 8, title: "Angular", date: "13 December 2023", image: Angular },
-  { id: 9, title: "DevOps for Web Development", date: "11 December 2023", image: DevTown },
+  { id: 1, title: "Web Designer for Beginner", date: "14 October 2023", image: webDesingn },
+  { id: 2, title: "GraphicDesign,UI/UX", date: "03 Jan 2024", image: simple },
+  { id: 3, title: "java", date: "15 Mar 2023", image: HackerJava },
+  { id: 4, title: "Android Application", date: "on September 2023", image: AndroidApplication },
+  { id: 5, title: "Front-end Developer", date: "13 july 2023", image: Frond },
+  { id: 6, title: "SQL", date: "14 February 2023", image: solosql },
+  { id: 7, title: "UI/UX", date: "on February 2023", image: uiux },
+  { id: 8, title: "SQL Basic", date: "28 February 2023", image: sqlhack },
+  { id: 9, title: "Introduction To AngularJS", date: "15 August 2023", image: AngularGl },
   { id: 10, title: "DevOps for Web Development", date: "11 December 2023", image: DevTown1 },
-  { id: 11, title: "Python Hacker Rank", date: "15 March 2023", image: Python },
-  { id: 12, title: "Javascript Hacker Rank", date: "18 August 2023", image: Javascript },
-  { id: 13, title: "Coding For Markerts", date: "18 August 2023", image: CodingForMarkert },
-  { id: 14, title: "React & Redux", date: "18 August 2023", image: ReactAndRedux },
-  { id: 15, title: "Responsive Web Design", date: "18 August 2023", image: ResponsiveWebDesign },
-  { id: 16, title: "SQL", date: "13 Feb 2023", image: SQL },
-  { id: 17, title: "Intoduction To Java", date: "13 Jan 2023", image: IntroductionTOJava },
-  { id: 18, title: "SQL", date: "13 Jan 2023", image: SQLSOLO },
-  { id: 19, title: "CSS", date: "13 Jan 2023", image: CSSSOLO },
-  { id: 20, title: "JavaScript", date: "13 Jan 2023", image: JsSolo },
-  { id: 21, title: "Java", date: "13 Jan 2023", image: JavaSolo },
-  { id: 22, title: "Front-end Developer", date: "13 Jan 2023", image: Front },
-  { id: 23, title: "Web Developer Internship", date: "13 Jan 2023", image: WebDev },
-  { id: 24, title: "Web Developer Internship", date: "13 Jan 2023", image: simple },
-  { id: 25, title: "Web Developer Internship", date: "13 Jan 2023", image: solo },
-  { id: 26, title: "Web Developer Internship", date: "13 Jan 2023", image: solosql },
-  { id: 27, title: "Web Developer Internship", date: "13 Jan 2023", image: sqlhack },
-  { id: 28, title: "Web Developer Internship", date: "13 Jan 2023", image: vistion },
-  { id: 28, title: "Web Developer Internship", date: "13 Jan 2023", image: Game },
-  { id: 28, title: "Web Developer Internship", date: "13 Jan 2023", image: HackerJava },
-  { id: 28, title: "Web Developer Internship", date: "13 Jan 2023", image: HackJavaScript },
-
-
-
-
+  { id: 11, title: "DevOps for Web Development", date: "11 December 2023", image: DevTown2 },
+  { id: 12, title: "HTML", date: "27 February 2023", image: solo },
+  { id: 13, title: "Building Games Using JavaSctript", date: "on August 2023", image: Billd_Game },
+  { id: 14, title: "Introduction To Desing Thinking", date: "on November 2023", image: DesingThinking},
+  { id: 15, title: "JavaSctript", date: "08 December 2023", image: HackJavaScript },
+  { id: 16, title: "Java", date: "09 December 2023", image: salolernJava },
+  { id: 17, title: "Server-side Web Programming", date: "16 August 2023", image: UOMWeb },
+  { id: 18, title: "JQuery Tutorial", date: "on September 2023", image: Jquriy },
+  { id: 19, title: "OOP in Java", date: "on February 2023", image: greatleaningoop },
+  { id: 20, title: "Data Structures in C", date: "on April 2023", image: DataStrat },
+  { id: 21, title: "JavaApplication", date: "on September 2023", image: JavaApplication},
+  { id: 22, title: "HTLM Attributes & Tags", date: "on August 2023", image:  HTMLTag },
+  { id: 23, title: "Intoduction To JavaSctript", date: "09 December 2023", image: JavaSctriptsolo },
+  { id: 24, title: "Introdution to DevOps", date: "on November 2023", image: DevOpsGl },
+  { id: 25, title: "Mongodb Tutorial", date: "on December 2023", image: Mongodb },
+  { id: 26, title: "Building Games Using Java ", date: "on October 2023", image: Game },
+  { id: 27, title: "Fornd-End Development CSS", date: "on February 2023", image: greatleaningcss},
+  { id: 28, title: "Java Programming", date: "on February 2023", image: greatleaningjava },
+  { id: 29, title: "visual studio online", date: "on September 2023", image: vistion },
+  { id: 30, title: "Coding Enviroment", date: "on February 2024", image: coding },
 ];
